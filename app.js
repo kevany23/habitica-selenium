3cfbae1e25d5c51f0866f818b4d5efd4382b5e9e
kevany23 <kevan23yang@gmail.com>
2021-10-03 02:02:16 -0500

    Added login setup

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const { SeleniumServer } = require('selenium-webdriver/remote');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://localhost:8080');
+    await driver.get('http://localhost:8080/login');
   } finally {
     //await driver.quit();
-    console.log("Connected");
+    try {
+      let usernameInput = await driver.findElement(By.id("usernameInput"));
+      let result = usernameInput.sendKeys("seleniumTester");
+      let passwordInput = await driver.findElement(By.id("passwordInput"));
+      result = passwordInput.sendKeys("dropper_123");
+      console.log(result);
+      let loginButton = await driver.findElement(By.css('button'));
+      loginButton.click();
+    }
+    catch(err) {
+      console.log("ERROR IN TESTING");
+      console.log(err);
+    }
   }
 })();
