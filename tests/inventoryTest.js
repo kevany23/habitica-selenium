7c6d03bacb60f6e377547f13ffb26b4980a87b12
kevany23 <kevan23yang@gmail.com>
2021-10-07 23:07:07 -0500

    Added task test cases

---
+++ tests/inventoryTest.js
@@ -0,0 +1,7 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { SeleniumServer } = require('selenium-webdriver/remote');
+const assert = require('assert');
+
+var runInventoryTests = function(driver) {
+  
+}
--- tests/taskTest.js
+++ tests/taskTest.js
@@ -2,7 +2,15 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const { SeleniumServer } = require('selenium-webdriver/remote');
 const assert = require('assert');
 
+var driver;
+
+// Keeps driver so helpers don't 
+function setDriver(dr) {
+  driver = dr;
+}
+
 var runTaskTests = function(driver) {
+  setDriver(driver);
   describe('Now Running tests on taskTest.js', function() {
     it('Example test', async function() {
       assert.equal(1, 1);
@@ -35,9 +43,75 @@ var runTaskTests = function(driver) {
           assert.equal(currHealth, 51);
         })
       });
+      describe('Now testing basic Task functionality', function() {
+        it('Starting with Basic Daily', async function() {
+          // First get current EXP Level
+          // Define function to get the element and value
+          let progressBars = await driver.findElements(
+            By.className('progress-container')
+          );
+          let expBar = progressBars[1];
+          async function getExp() {
+            let expString = await expBar.getText();
+            // now we have intial exp to test the values
+            let exp = parseInt(expString.split('/')[0]);
+            return exp;
+          }
+          // now we have intial exp to test the values
+          let initialExp = await getExp();
+
+          let dailyCheckbox = await driver.findElement(
+            By.className('task-control daily-todo-control')
+          );
+          // click here works
+          await dailyCheckbox.click();
+          // Check the functionality
+          let currExp = await getExp();
+          assert.equal(currExp, initialExp + 6,
+            'Checking if exp updates properly');
+          
+          // Revert the task completion
+          await dailyCheckbox.click();
+          currExp = await getExp();
+          assert.equal(currExp, initialExp, 'Testing daily task uncheck');
+
+        })
+      });
+      describe('Testing Habits', function() {
+        it('Good Habit Test', async function() {
+          let startHealth = await getHealth();
+          let habitBar = await driver.findElement(
+            By.className('task-control habit-control habit-control-negative-enabled')
+          );
+          habitBar.click();
+          let currHealth = await getHealth();
+          // This is actually harder to test than expected
+          // For now just check if it decreases health
+          assert.equal(currHealth < startHealth , true);
+        })
+      });
     });
   });
   return 1;
+}
+
+/**
+ * Common helpers here
+ * 
+ * This includes taking commonly used elements and their values
+ */
+
+async function getHealth() {
+  let healthDisplay = await driver.findElement(By.xpath(
+    "//span[contains(text(), '/ 50')]"
+  ));
+  let healthText = await healthDisplay.getText();
+  let currHealth = parseInt(healthText.split('/')[0]);
+  return currHealth;
+}
+
+function getExp() {
+
 }
 
 module.exports = {
